62d07a5edf80a1ec57ac19c92f7f8dbd8a6e979f
christinajoice <christinallrejoice@gmail.com>
2019-01-30 14:13:58 +0530

    Update and rename Wordcount.java to SpaceCount.java

--- SpaceCount.java
+++ SpaceCount.java
@@ -1,21 +1,15 @@
 import java.io.*;
 import java.util.*;
-public class Wordcount
+public class SpaceCount
 {
 public static void main(String args[])
 {
-int count=0;
 Scanner s=new Scanner(System.in);
 String str;
 System.out.println("Enter a String");
 str=s.nextLine();
-for(int i=0;i<str.length();i++)
-{
-if(str.indexOf(" ")==-1)
-{
-count++;
-}
-}
-System.out.println("The number of words are"+count);
+String[] word=str.split("\\s");
+  int n=word.length-1;
+  System.out.println(n+" spaces");
 }
 }
